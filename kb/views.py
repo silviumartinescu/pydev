254f70bba91e33749738a8887a8bde70cd68f3f0
Silviu Martinescu <martinescu.silviu28@gmail.com>
2022-03-19 17:57:39 +0200

    Create Article model and migrate to database

--- kb/views.py
+++ kb/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Article
 # Create your views here.
 
 
 def home(request):
     # return HttpResponse('<h1>Knowledge base - homepage </h1>')
-    articles = [
-        {
-            'author': 'John Doe',
-            'title': 'How to start development server',
-            'content': 'python manage.py runserver',
-            'date': 'December 4, 2021'
-        },
-        {
-            'author': 'Max Doe',
-            'title': 'How to stop the development server',
-            'content': 'Ctrl-C',
-            'date': 'December 4, 2021'
-        }
-    ]
-    context = {'title': 'Articles', 'articles': articles}
+    # articles = [
+    #     {
+    #         'author': 'John Doe',
+    #         'title': 'How to start development server',
+    #         'content': 'python manage.py runserver',
+    #         'date': 'December 4, 2021'
+    #     },
+    #     {
+    #         'author': 'Max Doe',
+    #         'title': 'How to stop the development server',
+    #         'content': 'Ctrl-C',
+    #         'date': 'December 4, 2021'
+    #     }
+    # ]
+    context = {'title': 'Articles', 'articles': Article.objects.all}
     return render(request, 'kb/home.html', context)
 
 
